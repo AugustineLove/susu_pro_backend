35f54921723024b1ae1aa90a1f6d56ed1b1fe84e
Augustine Love Stephens <106684513+AugustineLove@users.noreply.github.com>
2025-08-25 17:27:06 +0000

    Add files via upload

---
+++ src/controllers/smsController.mjs
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const sendCustomerMessage = async (req, res) => {
+    console.log("sending message")
+    const { messageTo, messageFrom, message } = req.body;
+    const data = {
+        "sender": messageFrom,
+        "message": message,
+        "recipients": [messageTo],
+      };
+
+        const config = {
+        method: 'post',
+        url: 'https://sms.arkesel.com/api/v2/sms/send',
+        headers: {
+        'api-key': process.env.ARKESEL_SMS_API_KEY
+        },
+        data : data
+        };
+
+    axios(config)
+    .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    res.status(200).json(response.data)
+    })
+    .catch(function (error) {
+    console.log(error);
+});
+
+}
+
+export default sendCustomerMessage;
