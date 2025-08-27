dcdece1248acf099c09779a55f5cc30b683a8ae9
Augustine Love Stephens <106684513+AugustineLove@users.noreply.github.com>
2025-08-27 01:09:15 +0000

    Update customerRoutes.mjs

--- src/routes/customerRoutes.mjs
+++ src/routes/customerRoutes.mjs
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCustomer, deleteCustomer, getCustomersByCompany, getCustomersByStaff } from "../controllers/customerController.mjs";
+import { createCustomer, deleteCustomer, getCustomersByCompany, getCustomersByStaff, udpateCustomerInfoMobile } from "../controllers/customerController.mjs";
 
 
 export const customerRouter = Router();
@@ -8,3 +8,4 @@ customerRouter.post('/create', createCustomer);
 customerRouter.get('/staff/:staffId', getCustomersByStaff);
 customerRouter.get('/company/:companyId', getCustomersByCompany);
 customerRouter.delete('/delete', deleteCustomer);
+customerRouter.put('/update-mobile/:customerId', udpateCustomerInfoMobile); 
