7eab55fd3cc497f82a43ddf6657491654ca4fe23
AugustineLove <augustinelovestephens@gmail.com>
2026-05-11 01:04:33 +0000

    Fixing deducting account for withdrawal

--- src/services/accountingHelper.mjs
+++ src/services/accountingHelper.mjs
@@ -145,7 +145,8 @@ export async function postJournalEntry(client, opts) {
 // payment method.  Returns the code string, not the uuid.
 // ─────────────────────────────────────────────────────────────
 export function cashCoaCode(paymentMethod, user) {
-  if (paymentMethod === "momo")  return "1010-03"; // MoMo float
+  console.log(paymentMethod, user);
+  if (paymentMethod === "momo")  return "1010-01"; // MoMo float
   if (paymentMethod === "bank")  return "1020-01"; // Bank account
   if(user === 'teller') return "1010-02"
   return "1010-01";                                // cash cash equivalent (default)
